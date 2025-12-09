079f696ebf794702e3ccc20fb9986cfea94ee1de
kakj-go <kakjzzw@gmail.com>
2025-12-09 18:09:56 +0800

    1. 并发处理

--- backend/internal/market/service/mcp_to_intelligent_task.go
+++ backend/internal/market/service/mcp_to_intelligent_task.go
@@ -319,69 +319,64 @@ func ProcessMcpToIntelligentTask(id int64) {
 
 	var logs = []*model.InstallLog{}
 	var lastStatus = pb.McpToIntelligentTaskStatus_Success.String()
+
 	var wg sync.WaitGroup
 	var mu sync.Mutex
 	concurrency := 10
 	sem := make(chan struct{}, concurrency)
 
 	for _, instanceID := range task.McpInstanceIDs {
-		wg.Add(1)
-		sem <- struct{}{}
-		go func(instanceID string) {
-			defer func() {
-				<-sem
-				wg.Done()
-			}()
-			// 获取实例信息
-			mcpInstance, err := mysql.McpInstanceRepo.FindByInstanceID(context.Background(), instanceID)
-			if err != nil {
-				mu.Lock()
-				logs = append(logs, &model.InstallLog{
-					McpInstanceID:   instanceID,
-					McpInstanceName: "",
-					Status:          false,
-					ErrorLog:        fmt.Sprintf("failed to find mcpInstance: %s", err.Error()),
-				})
-				lastStatus = pb.McpToIntelligentTaskStatus_Failed.String()
-				if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
-					logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
-				}
-				mu.Unlock()
-				return
+		// 获取实例信息
+		mcpInstance, err := mysql.McpInstanceRepo.FindByInstanceID(context.Background(), instanceID)
+		if err != nil {
+			logs = append(logs, &model.InstallLog{
+				McpInstanceID:   instanceID,
+				McpInstanceName: "",
+				Status:          false,
+				ErrorLog:        fmt.Sprintf("failed to find mcpInstance: %s", err.Error()),
+			})
+			lastStatus = pb.McpToIntelligentTaskStatus_Failed.String()
+			if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
+				logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
 			}
+			continue
+		}
 
-			// 获取实例令牌
-			_, err = mysql.McpTokenRepo.ListByInstanceID(context.Background(), mcpInstance.InstanceID)
-			if err != nil {
-				mu.Lock()
-				logs = append(logs, &model.InstallLog{
-					McpInstanceID:   instanceID,
-					McpInstanceName: "",
-					Status:          false,
-					ErrorLog:        fmt.Sprintf("failed to find mcpInstance: %s", err.Error()),
-				})
-				lastStatus = pb.McpToIntelligentTaskStatus_Failed.String()
-				if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
-					logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
-				}
-				mu.Unlock()
-				return
+		// 获取实例令牌
+		tokens, err := mysql.McpTokenRepo.ListByInstanceID(context.Background(), mcpInstance.InstanceID)
+		if err != nil {
+			logs = append(logs, &model.InstallLog{
+				McpInstanceID:   instanceID,
+				McpInstanceName: "",
+				Status:          false,
+				ErrorLog:        fmt.Sprintf("failed to find mcpInstance: %s", err.Error()),
+			})
+			lastStatus = pb.McpToIntelligentTaskStatus_Failed.String()
+			if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
+				logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
 			}
+			continue
+		}
 
-			log := &model.InstallLog{
-				McpInstanceID:         instanceID,
-				McpInstanceName:       mcpInstance.InstanceName,
-				Status:                true,
-				ErrorLog:              "",
-				InsertIntelligentLogs: nil,
-			}
-			mu.Lock()
-			logs = append(logs, log)
-			mu.Unlock()
-			// 记录每条 space 的插入日志
-			var insertIntelligentLogs []*model.InsertIntelligentLog
-			// 遍历插入信息列表
-			for _, insertInfo := range task.InsertIntelligentInfos {
+		log := &model.InstallLog{
+			McpInstanceID:         instanceID,
+			McpInstanceName:       mcpInstance.InstanceName,
+			Status:                true,
+			ErrorLog:              "",
+			InsertIntelligentLogs: nil,
+		}
+		logs = append(logs, log)
+		// 记录每条 space 的插入日志
+		var insertIntelligentLogs []*model.InsertIntelligentLog
+		// 遍历插入信息列表
+		for _, insertInfo := range task.InsertIntelligentInfos {
+			go func(insertInfo *model.InsertIntelligentInfo) {
+				defer wg.Done()
+				sem <- struct{}{}
+				defer func() {
+					<-sem
+				}()
+
 				searchTask, err := mysql.McpToIntelligentTaskRepo.FindByID(context.Background(), id)
 				if err != nil {
 					logger.Error(fmt.Sprintf("failed to find mcp to intelligent task: %s", err.Error()), zap.Int64("taskId", id))
@@ -395,6 +390,7 @@ func ProcessMcpToIntelligentTask(id int64) {
 				// 执行创建 dify tools
 				err = createDifyTools(instanceID, task.Domain, insertInfo, mcpInstance, userSpaces, conn)
 				if err != nil {
+					mu.Lock()
 					insertIntelligentLogs = append(insertIntelligentLogs, &model.InsertIntelligentLog{
 						InsertIntelligentInfo: insertInfo,
 						Status:                false,
@@ -404,30 +400,53 @@ func ProcessMcpToIntelligentTask(id int64) {
 					log.Status = false
 					log.ErrorLog = err.Error()
 					log.InsertIntelligentLogs = insertIntelligentLogs
-					mu.Lock()
+
 					lastStatus = pb.McpToIntelligentTaskStatus_Failed.String()
 					if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
 						logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
 					}
 					mu.Unlock()
-				} else {
-					insertIntelligentLogs = append(insertIntelligentLogs, &model.InsertIntelligentLog{
-						InsertIntelligentInfo: insertInfo,
-						Status:                true,
-						ErrorLog:              "",
-					})
-					log.InsertIntelligentLogs = insertIntelligentLogs
-					mu.Lock()
-					if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
-						logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
-					}
-					mu.Unlock()
+					return
 				}
-			}
-		}(instanceID)
-	}
-	wg.Wait()
 
+				if insertInfo.Headers[instanceID] != nil {
+					err = createOrUpdateInstanceToken(instanceID, insertInfo, task, intelligentAccess, tokens)
+					if err != nil {
+						mu.Lock()
+						insertIntelligentLogs = append(insertIntelligentLogs, &model.InsertIntelligentLog{
+							InsertIntelligentInfo: insertInfo,
+							Status:                false,
+							ErrorLog:              err.Error(),
+						})
+						log.Status = false
+						log.ErrorLog = err.Error()
+						log.InsertIntelligentLogs = insertIntelligentLogs
+
+						lastStatus = pb.McpToIntelligentTaskStatus_Failed.String()
+						if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
+							logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
+						}
+						mu.Unlock()
+						return
+					}
+				}
+
+				mu.Lock()
+				insertIntelligentLogs = append(insertIntelligentLogs, &model.InsertIntelligentLog{
+					InsertIntelligentInfo: insertInfo,
+					Status:                true,
+					ErrorLog:              "",
+				})
+				log.InsertIntelligentLogs = insertIntelligentLogs
+				if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, pb.McpToIntelligentTaskStatus_Running.String()); err != nil {
+					logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
+				}
+				mu.Unlock()
+			}(insertInfo)
+		}
+		wg.Wait()
+	}
+	// 最后更新整个任务的状态
 	if err = mysql.McpToIntelligentTaskRepo.UpdateLogs(context.Background(), id, logs, lastStatus); err != nil {
 		logger.Error(fmt.Sprintf("failed to update mcp to intelligent task logs: %s", err.Error()), zap.Int64("taskId", id))
 	}
